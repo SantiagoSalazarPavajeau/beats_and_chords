e9dcbaa3e5620ca4d9ac657dc23037c12fbd8618
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-20 15:52:10 +0200

    add button to body

--- frontend/index.js
+++ frontend/index.js
@@ -3,3 +3,11 @@ document.addEventListener("DOMContentLoaded",function(){
 })
 
 
+let addButton = function(){
+    let body = document.querySelector("body")
+    let button = document.createElement("button")
+    button.innerText = "A"
+    body.appendChild(button)
+    console.log(body)
+}
+
