c60625a15e2841fd28833f9b22ef371fbcd7f679
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-20 16:04:20 +0200

    add audio tag

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,19 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded",() =>{
     addButton();
 })
 
 
 let addButton = function(){
     let body = document.querySelector("body")
-    let button = document.createElement("button")
-    button.innerText = "A"
+    let playButton = document.createElement("button")
+    let audio = document.createElement("audio")
+    audio.setAttribute("src", "https://freewavesamples.com/files/Roland-JV-2080-101-Bass-C2.wav")
+    button.innerText = "C"
+    button.addEventListener("click", ()=> {
+
+    })
     body.appendChild(button)
     console.log(body)
 }
 
+// add event listener to button to play 
