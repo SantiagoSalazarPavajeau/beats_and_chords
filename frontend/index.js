1a03debf0324d484816c98117b32f029241a349e
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-20 16:56:00 +0200

    add file hosted in google drive

--- frontend/index.js
+++ frontend/index.js
@@ -21,7 +21,7 @@ let buttonG = function(){
     let body = document.querySelector("body")
     let playButton = document.createElement("button")
     let audio = document.createElement("audio") // working without adding it to dom
-    audio.setAttribute("src", "https://freewavesamples.com/files/Ensoniq-ZR-76-Harpsichord-C3.wav")
+    audio.setAttribute("src", "https://drive.google.com/uc?export=view&id=15-BxDTfMjt0TLs5ZfSvf7DpkExHNe0_c")
     playButton.innerText = "G"
     playButton.addEventListener("click", ()=> {
         audio.play()
