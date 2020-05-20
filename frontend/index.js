0caf59230701d0b495d942a73688919e44827e64
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-20 16:36:48 +0200

    add button w/ different sound

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,31 @@
 document.addEventListener("DOMContentLoaded",() =>{
-    addButton();
+    buttonC();
+    buttonG();
 })
 
 
-let addButton = function(){
+let buttonC = function(){
     let body = document.querySelector("body")
     let playButton = document.createElement("button")
-    let audio = document.createElement("audio")
-    audio.setAttribute("src", "https://freewavesamples.com/files/Roland-JV-2080-101-Bass-C2.wav")
+    let audio = document.createElement("audio") // working without adding it to dom
+    audio.setAttribute("src", "https://freewavesamples.com/files/Casio-VZ-10M-Astral-C2.wav")
     playButton.innerText = "C"
     playButton.addEventListener("click", ()=> {
         audio.play()
-    })
+    }) // add event listener to button to play 
     body.appendChild(playButton)
     console.log(body)
 }
 
-// add event listener to button to play 
+let buttonG = function(){
+    let body = document.querySelector("body")
+    let playButton = document.createElement("button")
+    let audio = document.createElement("audio") // working without adding it to dom
+    audio.setAttribute("src", "https://freewavesamples.com/files/Ensoniq-ZR-76-Harpsichord-C3.wav")
+    playButton.innerText = "G"
+    playButton.addEventListener("click", ()=> {
+        audio.play()
+    }) // add event listener to button to play 
+    body.appendChild(playButton)
+    console.log(body)
+}
