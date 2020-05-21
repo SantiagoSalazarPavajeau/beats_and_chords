14249ff292f3348e91e192267527c99ce043433d
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-21 11:24:01 +0200

    add g new song class-backend

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,10 @@ document.addEventListener("DOMContentLoaded",() =>{
     buttonC();
     buttonG();
     buttonAll();
+    pauseButton();
 })
+
+
 
 
 let buttonC = function(){
@@ -13,11 +16,12 @@ let buttonC = function(){
     playButton.innerText = "C"
     playButton.addEventListener("click", ()=> {
         audio.play()
-    }) // add event listener to button to play 
+    }) // add event listener to button to play
     body.appendChild(playButton)
     body.appendChild(audio)
 }
 
+//this function could add the audio played into an array for playing
 let buttonG = function(){
     let body = document.querySelector("body")
     let playButton = document.createElement("button")
@@ -34,14 +38,39 @@ let buttonG = function(){
 let buttonAll = function(){
     let body = document.querySelector("body")
     let playButton = document.createElement("button")
-    let audio = document.createElement("audio") // working without adding it to dom
-    audio.setAttribute("src", "https://drive.google.com/uc?export=view&id=15-BxDTfMjt0TLs5ZfSvf7DpkExHNe0_c")
     playButton.innerText = "All"
     playButton.addEventListener("click", ()=> {
+        // set timeout to play audios in order
         let audios = document.querySelectorAll("audio")
-        for(let audio in audios){
-            audios[audio].play() // play multiple audios
-        }
+        audios[0].play()
+        setInterval(playAudios, 4000, audios)
+        
     }) // add event listener to button to play 
     body.appendChild(playButton)
 }
+
+let pauseButton = function(){
+    let body = document.querySelector("body")
+    let pauseButton = document.createElement("button")
+    pauseButton.innerText = "Stop"
+    pauseButton.addEventListener("click", ()=> {
+        // set timeout to play audios in order
+       clearInterval()
+       count = 1
+        
+    }) // add event listener to button to play 
+    body.appendChild(pauseButton)
+}
+let count = 1
+
+// this function could play any audios from an array in any order
+let playAudios = function(audios) {
+    if (count < audios.length){
+        audios[count].play()
+        count += 1
+    }  else{
+        clearInterval()
+    } // play multiple audios
+     // set timer for each loop: 
+} // improve so count is stopped after all sounds are played
+// improve so sound is played from start of interval
