479b5b94cd808b3a35aef93657e72cf0826f20d6
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-22 14:40:17 +0200

    separate chords class into new file

--- frontend/chord.js
+++ frontend/chord.js
@@ -1,4 +1,4 @@
-export default class Chord{
+class Chord{
     constructor(name, file){
         this.name = name
         this.file = file
@@ -22,3 +22,4 @@ export default class Chord{
 
 
 }
+
--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,5 @@
 // import Chord from './chord.js'
+let chordData = ["80s EP_L-01.wav ", "Tyrol Mountain_L-01.wav ", "bliss pad_L-01.wav ", "chunky house_L-01.wav ", "easy trailer_L-01.wav ", "echolette retrodub_L-01.wav ", "ghetto strings_L-01.wav ", "jazzman EP_L-01.wav ", "jb is dead_L-01.wav ", "jungle choir_L-01.wav ", "pop piano strings_L-01.wav ", "retro swing_L-01.wav ", "retro talk pad_L-01.wav ", "so very smooth_L-01.wav ", "suspense scifi_L-01.wav ", "vintage renaissance_L-01.wav ", "wurli ersatz_L-01.wav"]
 
 
 document.addEventListener("DOMContentLoaded",() =>{
@@ -9,7 +10,6 @@ document.addEventListener("DOMContentLoaded",() =>{
     addChords(chordData);
 })
 
-let chordData = ["80s EP_L-01.wav ", "Tyrol Mountain_L-01.wav ", "bliss pad_L-01.wav ", "chunky house_L-01.wav ", "easy trailer_L-01.wav ", "echolette retrodub_L-01.wav ", "ghetto strings_L-01.wav ", "jazzman EP_L-01.wav ", "jb is dead_L-01.wav ", "jungle choir_L-01.wav ", "pop piano strings_L-01.wav ", "retro swing_L-01.wav ", "retro talk pad_L-01.wav ", "so very smooth_L-01.wav ", "suspense scifi_L-01.wav ", "vintage renaissance_L-01.wav ", "wurli ersatz_L-01.wav"]
 
 // start organizing by classes
 // classes can build front end(adapter and objects)?
@@ -18,46 +18,12 @@ let addChords = function(chordData){
     chordData.map((string)=> {
         let chord = new Chord(string.substring(0, string.length - 10), `assets/chords/${string}`)
         chord.addChordButton()
-    }) //map  over the chord data array to create a new chord from the element array and call the method to add a button per chord
-}
-class Chord{
-    constructor(name, file){
-        this.name = name
-        this.file = file
-    }
-
-    addChordButton(){ 
-        let chordsCard = document.getElementById("chords")
-        let playButton = document.createElement("a")
-        //  <a href="#" class="btn btn-info">C</a>
-        playButton.className = "btn btn-info"
-        playButton.href = "#" 
-        let audio = document.createElement("audio") // working without adding it to dom
-        audio.setAttribute("src", this.file) // create chords from array of chords data
-        playButton.innerText = this.name
-        playButton.addEventListener("click", ()=> {
-            audio.play()
-        }) // add event listener to button to play
-        chordsCard.appendChild(playButton) // could add it to a list to fix spacing
-        chordsCard.appendChild(audio)
-    }
-
-
+    }) 
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+ //map  over the chord data array to create a 
+    //new chord from the element array 
+    //and call the method to add a button per chord
 
 
 let synthWave = function(){
@@ -80,36 +46,6 @@ let synthWaveButton = function(json){
     }) // add event listener to button to play
     songsCard.appendChild(playButton)
     // div.appendChild(audio)
-}
-
-
-
-
-let buttonC = function(){
-    let chordsCard = document.getElementById("chords")
-    let playButton = document.createElement("button")
-    let audio = document.createElement("audio") // working without adding it to dom
-    audio.setAttribute("src", "https://freewavesamples.com/files/Casio-VZ-10M-Astral-C2.wav")
-    playButton.innerText = "C"
-    playButton.addEventListener("click", ()=> {
-        audio.play()
-    }) // add event listener to button to play
-    chordsCard.appendChild(playButton)
-    chordsCard.appendChild(audio)
-}
-
-//this function could add the audio played into an array for playing
-let buttonG = function(){
-    let chordsCard = document.getElementById("chords")
-    let playButton = document.createElement("button")
-    let audio = document.createElement("audio") // working without adding it to dom
-    audio.setAttribute("src", "https://drive.google.com/uc?export=view&id=15-BxDTfMjt0TLs5ZfSvf7DpkExHNe0_c")
-    playButton.innerText = "G"
-    playButton.addEventListener("click", ()=> {
-        audio.play()
-    }) // add event listener to button to play 
-    chordsCard.appendChild(playButton)
-    chordsCard.appendChild(audio)
 }
 
 function playAudios(audios) {
