826a376144be467e4dd1e3a80d7b7b7777b619c6
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-25 13:06:04 +0200

    rename index.js functions

--- frontend/chord.js
+++ frontend/chord.js
@@ -34,12 +34,16 @@ class Chord{
                 minus.innerHTML = `<svg class="bi bi-dash-square-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm2 7.5a.5.5 0 000 1h8a.5.5 0 000-1H4z" clip-rule="evenodd"/>
                                  </svg>`
+
                 chordButtonTrack.appendChild(minus)
                 trackCard.appendChild(chordButtonTrack)
                 chordButtonTrack.addEventListener("click", (e)=>{
                     chordButtonTrack.parentNode.removeChild(chordButtonTrack)
                     audio.pause()
                     audio.currentTime = 0
+                    // add audio to song
+                    // song could be an array
+                    
                 })
             
             }) // add event listener to button to play
@@ -51,35 +55,8 @@ class Chord{
             // div.appendChild(playButton)
             chordsCard.appendChild(audio)
             chordButton.appendChild(addIcon)
-        // if(card === "track"){
-        //     let chordsCard = document.getElementById(card)
-        //     // let div = document.createElement("")
-        //     let playButton = document.createElement("button")
-        //      //  <a href="#" class="btn btn-info">C</a>
-        //      playButton.className = "button btn-info"
-        //      playButton.href = "#"
-        //      playButton.innerText = this.name
-             
-        //     let add = document.createElement("span")
-        //     add.innerHTML = `<svg class="bi bi-dash-square-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        //                         <path fill-rule="evenodd" d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm2 7.5a.5.5 0 000 1h8a.5.5 0 000-1H4z" clip-rule="evenodd"/>
-        //                      </svg>`
-    
-        //     let audio = document.createElement("audio") // working without adding it to dom
-        //     audio.setAttribute("src", this.file) // create chords from array of chords data
-    
-        //     playButton.addEventListener("click", ()=> {
-        //         audio.play()
-        //     }) // add event listener to button to play
-    
-           
-           
-        //     chordsCard.appendChild(playButton) // could add it to a list to fix spacing
-        //     // div.appendChild(playButton)
-        //     chordsCard.appendChild(audio)
-        //     playButton.appendChild(add)
-        // }
-        
+            
+            // add chord to song array
 
     }
 
--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,4 @@
-// import Chord from './chord.js'
 let chordData = ["80s EP_L-01.wav ", "Tyrol Mountain_L-01.wav ", "bliss pad_L-01.wav ", "chunky house_L-01.wav ", "easy trailer_L-01.wav ", "echolette retrodub_L-01.wav ", "ghetto strings_L-01.wav ", "jazzman EP_L-01.wav ", "jb is dead_L-01.wav ", "jungle choir_L-01.wav ", "pop piano strings_L-01.wav ", "retro swing_L-01.wav ", "retro talk pad_L-01.wav ", "so very smooth_L-01.wav ", "suspense scifi_L-01.wav ", "vintage renaissance_L-01.wav ", "wurli ersatz_L-01.wav"]
-
-
-document.addEventListener("DOMContentLoaded",() =>{
-    
-    buttonAll();
-    pauseButton();
-    synthWave();
-    addChords(chordData);
-})
-
 
 // start organizing by classes
 // classes can build front end(adapter and objects)?
@@ -19,47 +8,30 @@ let addChords = function(chordData){
         let chord = new Chord(`${string.substring(0, string.length - 10)} `, `assets/chords/${string}`)
         chord.addChordButton("chords")
     })
-    // need to add a new button to the track so have to instantiate a button inside this event listener
-    // gotta find all add span tags in dom
-    // let chordButtons = document.querySelectorAll("button")
-
-    // for(let button of chordButtons){
-    //     button.addEventListener("click",(e) =>{
-    //         let name = e.target.parentNode.parentNode.parentNode.innerText // getting bug adding text of all card
-    //         let file = e.target.parentNode.parentNode.parentNode.innerText
-    //         file = `${file.substring(0, file.length -1)}_L-01.wav `
-    //         let trackChord = new Chord(name, `assets/chords/${file}`)
-    //         trackChord.addChordButton("track")
-    //     })
-    // }
     
 };
 
- //map  over the chord data array to create a 
-    //new chord from the element array 
-    //and call the method to add a button per chord
+document.addEventListener("DOMContentLoaded",() =>{
+    addChords(chordData);
+
+    
+    renderPlayButton();
+    pauseButton();
+    renderSongs();
+})
 
 
-let synthWave = function(){
-    fetch("http://localhost:3000/songs/1")
-        .then(response => response.json())
-        .then(json =>synthWaveButton(json))// I am getting a string not an array so have to create a model in backend
-}
-// create song button with response json
 
-let synthWaveButton = function(json){
-    let songsCard = document.getElementById("songs")
+let renderPlayButton = function(){
+    let trackBtns = document.getElementById("track-btns")
     let playButton = document.createElement("button")
-    let audio0 = document.createElement("audio")
-    let audio1 = document.createElement("audio")
-    audio0.setAttribute("src", json.chords[0].file) // src is first element in array
-    audio1.setAttribute("src", json.chords[1].file)
-    playButton.innerText = json.name
+    playButton.innerText = "Play"
     playButton.addEventListener("click", ()=> {
-        playAudios([audio0, audio1])
-    }) // add event listener to button to play
-    songsCard.appendChild(playButton)
-    // div.appendChild(audio)
+        let audios = document.querySelectorAll("audio") //select all audios from the track card or use the song array
+        playAudios(audios)
+        
+    }) // add event listener to button to play 
+    trackBtns.appendChild(playButton)
 }
 
 function playAudios(audios) {
@@ -76,17 +48,29 @@ function play(audio) {
     });
 }
 
-let buttonAll = function(){
-    let trackBtns = document.getElementById("track-btns")
-    let playButton = document.createElement("button")
-    playButton.innerText = "Play"
-    playButton.addEventListener("click", ()=> {
-        let audios = document.querySelectorAll("audio")
-        playAudios(audios)
-        
-    }) // add event listener to button to play 
-    trackBtns.appendChild(playButton)
+let renderSongs = function(){
+    fetch("http://localhost:3000/songs/1")
+        .then(response => response.json())
+        .then(json =>songButton(json))// I am getting a string not an array so have to create a model in backend
 }
+// create song button with response json
+
+let songButton = function(json){
+    let songsCard = document.getElementById("songs")
+    let playButton = document.createElement("button")
+    let audio0 = document.createElement("audio")
+    let audio1 = document.createElement("audio")
+    audio0.setAttribute("src", json.chords[0].file) // src is first element in array
+    audio1.setAttribute("src", json.chords[1].file)
+    playButton.innerText = json.name
+    playButton.addEventListener("click", ()=> {
+        playAudios([audio0, audio1])
+    }) // add event listener to button to play
+    songsCard.appendChild(playButton)
+    // div.appendChild(audio)
+}
+
+
 
 
 
