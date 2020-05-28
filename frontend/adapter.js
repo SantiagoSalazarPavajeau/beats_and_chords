3523c745f82bf378f6e2ca5480c21ed98de4657e
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-28 13:41:32 +0200

    create add songs fetch on adapter

--- frontend/adapter.js
+++ frontend/adapter.js
@@ -1,11 +1,17 @@
 class Adapter{
     constructor(){
-        this.baseURL = "http://localhost:3000/"
-        // this.getSongs()
+        this.baseURL = "http://localhost:3000"
+        this.getSongs()
         // this.saveSong()
         // this.getSong()
         // this.updateSong()
         // this.deleteSong()
+    }
+
+    getSongs(){
+        return fetch(`${this.baseURL}/songs`)
+                    .then(resp => resp.json())
+                    .then(json => console.log(json))
     }
 
     
