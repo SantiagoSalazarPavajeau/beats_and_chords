060c4b097a6b6cf73424eca2cc991f5aa5d2ed08
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-05-28 13:29:50 +0200

    add get chords request to adapter

--- frontend/adapter.js
+++ frontend/adapter.js
@@ -1,13 +1,23 @@
 class Adapter{
     constructor(){
+        this.baseURL = "http://localhost:3000/"
         this.getChords()
-        this.getSongs()
-        this.saveSong()
-        this.getSong()
-        this.updateSong()
-        this.deleteSong()
+        // this.getSongs()
+        // this.saveSong()
+        // this.getSong()
+        // this.updateSong()
+        // this.deleteSong()
     }
 
-    
+    getChords(){
+        return fetch(`${this.baseURL}songs`)
+                    .then(response => response.json())
+                    .then(json=> console.log(json))
+    }
+
 
 }
+
+const adapter = new Adapter
+
+// console.log(adapter.getChords())
